c67970708934a767d2bf6824d253751758cb6193
DieuLinh <ntdl19062003@gmail.com>
2024-10-04 23:48:48 +0700

    init commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# flutter_doctor_nearby
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/flutter_doctor_nearby/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flutter_doctor_nearby
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
